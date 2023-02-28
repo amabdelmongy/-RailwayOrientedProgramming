dde96358b5b5e23b354783c6bce3d3fa7d281a65
ahmed.ali <amabdelmongy@gmail.com>
2023-02-28 11:40:25 +0100

    reformat the code

--- FSharpSyntax/Program.fs
+++ FSharpSyntax/Program.fs
@@ -1,4 +1,5 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
+
 // "Variables" (but not really)
 let myInt = 5.                              // The "let" keyword defines an (immutable) value
 let myFloat = 3.14
@@ -23,7 +24,7 @@ let squareResult = square 3                 // Now run the function. Again, no p
 printfn $"square 3 = %i{squareResult}"
 let sumOfSquares n =
    [1..n] |> List.map square |> List.sum
-printfn $"sumOfSquares 1 to 4 = %i{sumOfSquares 4}" // 1 + 4 + 9 + 16
+printfn $"sumOfSquares 1 to 4 = %i{sumOfSquares 4}" // 1 + 4 + 9 + 16 = 30
 let add x y = x + y                         // don't use add (x,y)! It means something completely different.
 let addResult = add 2 3                     // Now run the function.
 printfn $"add 2 3 = %i{addResult}"
@@ -32,26 +33,10 @@ printfn $"add 2 3 = %i{addResult}"
 let sumOfSquaresTo100piped = 3 |> add 2 |> square             // output will be 25
 printfn $"3 |> add 2 |> square = %i{sumOfSquaresTo100piped}"
 
-// ======== rec =====================
-// Fibonacci
-// F{1} = F{2} =1
-// F{n} = F{n-1} + F{n-2} is valid for n > 2
-// number ->  1, 2, 3, 4, 5, 6, 7
-// result ->  1, 1, 2, 3, 5, 8, 13
-let rec Fibonacci n =
-    match n with
-    | 1
-    | 2 -> 1
-    | n -> Fibonacci(n-1) + Fibonacci(n-2)
-printfn $"Fibonacci 7 = %i{Fibonacci 7}"
-
+// ======== Record types =====================
 // Record types have named fields. Semicolons are separators. //C# now has record
 type Person = { First:string; Last:string}
-let person1 = {
-                    Person.First = "john";
-                    Last = "Doe"
-                }
-
+let person1 = { Person.First = "john"; Last = "Doe" }
 
 // ======== Discriminated Unions =====================
 // Union types have choices. Vertical bars are separators.
@@ -59,20 +44,20 @@ type Shape =
     | Rectangle of height : float * width : float
     | Circle of radius : float
 
+let circle = Shape.Circle 100
+let rectangle = Shape.Rectangle (200,50)
 
 // ======== Pattern Matching ========
 // Match..with.. is a supercharged case/switch statement.
 let matchShape shape =
-        match shape with
-        | Rectangle(height = h) -> printfn $"Rectangle with length %f{h}"
-        | Circle(r) -> printfn $"Circle with radius %f{r}"
-        | _ -> printfn " shape is something else"   // underscore matches anything
+    match shape with
+    | Rectangle(h,_) -> printfn $"Rectangle with length %f{h}"
+    | Circle(r) -> printfn $"Circle with radius %f{r}"
+    | _ -> printfn " shape is something else"   // underscore matches anything
 
-let bigCircle = Shape.Circle 100
-let bigRectangle = Shape.Rectangle (200,50)
-
-matchShape bigCircle
-matchShape bigRectangle
+// We want to know the type of the shape
+matchShape rectangle
+matchShape circle
 
 // In F# returns are implicit -- no "return" needed. A function always
 // returns the value of the last expression used.
