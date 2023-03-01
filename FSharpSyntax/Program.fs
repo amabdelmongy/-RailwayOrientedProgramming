b885005fea279ddf195ff5c7f3481c8ce1e77664
ahmed.ali <amabdelmongy@gmail.com>
2023-03-01 10:35:29 +0100

    Add more spaces

--- FSharpSyntax/Program.fs
+++ FSharpSyntax/Program.fs
@@ -11,6 +11,7 @@ let twoToFive = [2;3;4;5]                   // Square brackets create a list wit
 let oneToFive = [1..5]
 printfn $"list 1 to 5 = %A{oneToFive}"
 
+
 // ========= Complex Data Types =========
 // Tuple types are pairs, triples, etc. Tuples use commas.
 let twoTuple = 1,2
@@ -19,12 +20,17 @@ let threeTuple = "a",2,true
 
 // ======== Functions ========
 // The "let" keyword also defines a named function.
+// In F# returns are implicit -- no "return" needed. A function always
+// returns the value of the last expression used.
 let square x = x * x                        // Note that no parens are used.
+
 let squareResult = square 3                 // Now run the function. Again, no parens.
 printfn $"square 3 = %i{squareResult}"
+
 let sumOfSquares n =
    [1..n] |> List.map square |> List.sum
 printfn $"sumOfSquares 1 to 4 = %i{sumOfSquares 4}" // 1 + 4 + 9 + 16 = 30
+
 let add x y = x + y                         // don't use add (x,y)! It means something completely different.
 let addResult = add 2 3                     // Now run the function.
 printfn $"add 2 3 = %i{addResult}"
@@ -33,10 +39,12 @@ printfn $"add 2 3 = %i{addResult}"
 let sumOfSquaresTo100piped = 3 |> add 2 |> square             // output will be 25
 printfn $"3 |> add 2 |> square = %i{sumOfSquaresTo100piped}"
 
+
 // ======== Record types =====================
 // Record types have named fields. Semicolons are separators. //C# now has record
 type Person = { First:string; Last:string}
 let person1 = { Person.First = "john"; Last = "Doe" }
+
 
 // ======== Discriminated Unions =====================
 // Union types have choices. Vertical bars are separators.
@@ -47,18 +55,15 @@ type Shape =
 let circle = Shape.Circle 100
 let rectangle = Shape.Rectangle (200,50)
 
+
 // ======== Pattern Matching ========
 // Match..with.. is a supercharged case/switch statement.
 let matchShape shape =
     match shape with
-    | Rectangle(h,_) -> printfn $"Rectangle with length %f{h}"
-    | Circle(r) -> printfn $"Circle with radius %f{r}"
+    | Rectangle(height,_) -> printfn $"Rectangle with length %f{height}"
+    | Circle(radius) -> printfn $"Circle with radius %f{radius}"
     | _ -> printfn " shape is something else"   // underscore matches anything
 
 // We want to know the type of the shape
 matchShape rectangle
 matchShape circle
-
-// In F# returns are implicit -- no "return" needed. A function always
-// returns the value of the last expression used.
-
